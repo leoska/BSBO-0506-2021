b7644905918e1db4a6b70be3425a262842dfbcdf
Leonid <Leo77551@gmail.com>
2022-10-29 10:26:34 +0300

    first commit
    // stack, pointer, bubble sort

---
+++ FirstLab/FirstLab/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLab
+{
+    internal class Item
+    {
+        public int value = 0;
+        public Item? prev = null;
+
+        public Item(int value, Item? prev)
+        {
+            this.value = value;
+            this.prev = prev;
+        }
+    }
+}
---
+++ FirstLab/FirstLab/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// stack, pointer, bubble sort
+using FirstLab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack values = new Stack();
+
+        values.Push(7);
+        values.Push(3);
+        values.Push(10);
+        values.Push(-7);
+        values.Push(1);
+
+        values.Print();
+
+        for (int i = 0; i < values.size; i++)
+        {
+            for (int j = 0; j < values.size - i - 1; j++)
+            {
+                if (values.Get(j) > values.Get(j + 1))
+                {
+                    int buf = values.Get(j + 1);
+                    values.Set(j + 1, values.Get(j));
+                    values.Set(j, buf);
+                }
+            }
+        }
+        
+        values.Print();
+    }
+}
+
